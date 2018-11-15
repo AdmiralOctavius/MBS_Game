56d0ed2e46a9be01d65984d823b03a944c3dcf4b
KrisTheHaw <36865296+KrisTheHaw@users.noreply.github.com>
2018-11-14 21:53:26 -0500

    Cambat, pLAYER, AND eNEMY
    Caps lol. added player stats and enemeny stats and then addeed to the combat

--- MBS_Project/Assets/Scripts/CombatScripts/CombatScript.cs
+++ MBS_Project/Assets/Scripts/CombatScripts/CombatScript.cs
@@ -4,14 +4,54 @@ using UnityEngine;
 
 public class CombatScript : MonoBehaviour
 {
+    //Im not calling GetComponent Every god damn time
+    int playerAttack;
+    int playerDeffense;
+    int playerSpeed;
+    int playerHp;
 
-	void Start ()
+    int enemyAttack;
+    int enemyDeffense;
+    int enemySpeed;
+    int enemyHp;
+
+    void Start ()
     {
-		
-	}
+        //its ok to set the stats her ebecuase they wont need to be kept since fights wont be right after another
+        playerAttack = GetComponent<PlayerStuff>().playerAttack;
+        playerDeffense = GetComponent<PlayerStuff>().playerDeffense;
+        playerSpeed = GetComponent<PlayerStuff>().playerSpeed;
+        playerHp = GetComponent<PlayerStuff>().playerHp;
+
+        enemyAttack = GetComponent<Enemy>().enemyAttack;
+        enemyDeffense = GetComponent<Enemy>().enemyDeffense;
+        enemySpeed = GetComponent<Enemy>().enemySpeed;
+        enemyHp = GetComponent<Enemy>().enemyHp;
+    }
 	
 	void Update ()
     {
 		
 	}
+
+    //Player attack function
+    int PlayerAttacking()
+    {
+        enemyHp -= playerAttack;
+        return enemyHp;
+    }
+
+    //Players Breath
+    int Breath()
+    {
+        playerHp += 5;
+        return playerHp;
+    }
+
+    //Enemey Attack Function
+    int EnemenyAttacking()
+    {
+        playerHp -= enemyAttack;
+        return playerHp;
+    }
 }
---
+++ MBS_Project/Assets/Scripts/CombatScripts/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int enemyAttack = 50;
+    public int enemyDeffense = 5;
+    public int enemySpeed = 10;
+    public int enemyHp = 50;
+
+    void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		
+	}
+}
