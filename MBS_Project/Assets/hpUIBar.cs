4179e45ab0859f628de546ade9d280548c337790
AdmiralOctavius <buildgamesnotbombs@gmail.com>
2018-11-15 23:03:37 -0500

    Created BattleHPBar prefab

--- MBS_Project/Assets/hpUIBar.cs
+++ MBS_Project/Assets/hpUIBar.cs
@@ -1,22 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class hpUIBar : MonoBehaviour {
 
     public float hptemp;
+    public Slider slideme;
 
 	// Use this for initialization
 	void Start () {
-        hptemp = 1;
+        hptemp = 100;
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
         if(hptemp != 0)
         {
-            hptemp = hptemp - 0.01f;
-            transform.localScale = new Vector3(hptemp, transform.localScale.y);
+            hptemp = hptemp - 1f;
+            slideme.value = hptemp;
         }
 
 	}
