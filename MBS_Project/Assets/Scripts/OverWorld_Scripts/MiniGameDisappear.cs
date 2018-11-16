a889c2c5e044d6673f493e4d0167aa60efc1887b
bay-ah-tou <bay-ah-tou@users.noreply.github.com>
2018-11-16 00:09:43 -0500

    Overworld 12:09a.m. 11/16/2018
    -The mini-game NPC disappears when the BeatMinigame variable is 1

---
+++ MBS_Project/Assets/Scripts/OverWorld_Scripts/MiniGameDisappear.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniGameDisappear : MonoBehaviour
+{
+
+    public GameObject stats;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(stats.GetComponent<PlayerVariables>().BeatMinigame == 1)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
