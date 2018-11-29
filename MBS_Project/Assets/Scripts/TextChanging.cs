9224c9ec2c3c586698478fa3699e07fbf536c826
HwachaTurret <antman1218@gmail.com>
2018-11-28 23:54:09 -0500

    text transistion

---
+++ MBS_Project/Assets/Scripts/TextChanging.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextChanging : MonoBehaviour {
+
+    public List<string> textHolder;
+
+    //string currentText;
+
+    int currentSpot = 0;
+
+    public Text textTab;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0))
+        {
+            currentSpot++;
+
+            //textHolder[currentSpot];
+            //current = textHolder[i];
+            
+
+            textTab.text = textHolder[currentSpot];
+        }
+	}
+}
